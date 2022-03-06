5cc0c20da84fcec60a411d9a041a21343939e2dd
Marina Khadieva <utabond89@gmail.com>
2022-03-06 14:18:50 +0300

    Добавила табы

---
+++ js/view.js
@@ -0,0 +1,5 @@
+export const WEATHER_UI = {
+	BTN_SEARCH: document.querySelector('.weather__btn--search'),
+	SAVED: document.querySelectorAll('.weather__city'),
+	TABS: document.querySelectorAll('.weather__tabs--btn'),
+}
---
+++ js/weather.js
@@ -0,0 +1,31 @@
+import { WEATHER_UI } from './view.js';
+
+WEATHER_UI.BTN_SEARCH.addEventListener('click', function () {
+	alert("Нажата кнопка Поиска");
+});
+
+for (let city of WEATHER_UI.SAVED) {
+	city.addEventListener('click', function (event) {
+		const cityName = event.target.textContent;
+		alert(cityName);
+
+	})
+}
+
+function deleteClassesActive(){
+    for (let tab of WEATHER_UI.TABS){
+        const isCLassActive = tab.className === 'weather__tabs--btn weather__tabs--btnActive';
+
+        if (isCLassActive){
+            tab.classList.remove('weather__tabs--btnActive');
+        }
+    }
+}
+
+for (let tab of WEATHER_UI.TABS){
+    tab.addEventListener('click', function (){
+        deleteClassesActive();
+		alert("Один из табов нажат");
+        tab.classList.add('weather__tabs--btnActive');
+    })
+}
