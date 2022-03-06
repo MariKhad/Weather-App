84c2aa3af5465a41cedab233a3f952f30c2b2b23
Marina Khadieva <utabond89@gmail.com>
2022-03-07 02:50:34 +0300

    Подгрузила now и details

--- js/view.js
+++ js/view.js
@@ -2,4 +2,13 @@ export const WEATHER_UI = {
 	BTN_SEARCH: document.querySelector('.weather__btn--search'),
 	SAVED: document.querySelectorAll('.weather__city'),
 	TABS: document.querySelectorAll('.weather__tabs--btn'),
+	SEARCH_INPUT: document.querySelector('.weather__city'),
+	NOW_TEMP: document.querySelector('.weather__now--temp'),
+	CITY_NAME: document.querySelectorAll('h3'),
+	NOW_IMG: document.querySelector('.weather__now--img'),
+	DETAILS_TEMP: document.querySelector('.details__temp'),
+	DETAILS_FEELS: document.querySelector('.details__feels'),
+	DETAILS_PRES: document.querySelector('.details__precepitations'),
+	DETAILS_SUNRISE: document.querySelector('.details__sunrise'),
+	DETAILS_SUNSET: document.querySelector('.details__sunset'),
 }
--- js/weather.js
+++ js/weather.js
@@ -1,47 +1,92 @@
 import { WEATHER_UI } from './view.js';
 
-WEATHER_UI.BTN_SEARCH.addEventListener('click', function () {
-	alert("Нажата кнопка Поиска");
-});
+const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
+const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f';
+let cityName;
 
 for (let city of WEATHER_UI.SAVED) {
 	city.addEventListener('click', function (event) {
-		const cityName = event.target.textContent;
-		alert(cityName);
-
+		cityName = event.target.textContent;
+		WEATHER_UI.SEARCH_INPUT.value = cityName;
+		WEATHER_UI.BTN_SEARCH.click();
 	})
 }
 
-function deleteClassesActive(){
-    for (let tab of WEATHER_UI.TABS){
-        const isCLassActive = tab.className === 'weather__tabs--btn weather__tabs--btnActive';
+function deleteClassesActive() {
+	for (let tab of WEATHER_UI.TABS) {
+		const isCLassActive = tab.className === 'weather__tabs--btn weather__tabs--btnActive';
 
-        if (isCLassActive){
-            tab.classList.remove('weather__tabs--btnActive');
-        }
-    }
+		if (isCLassActive) {
+			tab.classList.remove('weather__tabs--btnActive');
+		}
+	}
 }
 
-for (let tab of WEATHER_UI.TABS){
-    tab.addEventListener('click', function (){
-        deleteClassesActive();
-        tab.classList.add('weather__tabs--btnActive');
-		  const href = tab.getAttribute('href').slice(1);
-		  const id = "#" + href;
-		  console.log("нажата клавиша");
-		  document.querySelectorAll('.active').forEach(el => el.classList.remove('active'));
-        document.querySelector(id).classList.add('active');
-    })
+function tempRound(a) {
+	let round = Math.round(+a - 273.15);
+	return round;
+}
+
+function addZero(time) {
+	if (time < 10) {
+		time = "0" + String(time);
+	}
+   return time;
 }
 
 
-/* вобщем, убери с табов класс weather__display, на первый добавь active или как тебе удобнее, 
-for (let tab of WEATHER_UI.TABS){
-    tab.addEventListener('click', function (e){
-        e.preventDefault()
-        deleteClassesActive();
-        tab.classList.add('weather__tabs--btnActive');
-        const href = tab.getAttribute('href').slice(1)
-        document.querySelector([id='${href}']).classList.add('active')
-    })
-} */
+
+for (let tab of WEATHER_UI.TABS) {
+	tab.addEventListener('click', function () {
+		deleteClassesActive();
+		tab.classList.add('weather__tabs--btnActive');
+		const href = tab.getAttribute('href').slice(1);
+		const id = "#" + href;
+		console.log("нажата клавиша");
+		document.querySelectorAll('.active').forEach(el => el.classList.remove('active'));
+		document.querySelector(id).classList.add('active');
+	})
+}
+
+WEATHER_UI.BTN_SEARCH.addEventListener('click', async function () {
+	cityName = (WEATHER_UI.SEARCH_INPUT.value).toLowerCase();
+	const url = `${serverUrl}?q=${cityName}&appid=${apiKey}`;
+	let response = await fetch(url);
+	if (response.ok) {
+		let json = await response.json();
+		let temp = json.main.temp;
+		temp = tempRound(temp);
+		WEATHER_UI.NOW_TEMP.textContent = temp + "°";
+		WEATHER_UI.DETAILS_TEMP.textContent = "Temperature: " + temp + "°";
+		let feels = json.main.feels_like;
+		feels = tempRound(feels);
+		WEATHER_UI.DETAILS_FEELS.textContent = "Feels like: " + feels + "°";
+		let pres = json.weather[0].main;
+		WEATHER_UI.DETAILS_PRES.textContent = "Weather: " + pres;
+		let rise = (json.sys.sunrise);
+		let rise_time = new Date(rise * 1000);
+		let rise_hours = rise_time.getHours();
+		rise_hours = addZero(rise_hours);
+		alert(rise_hours);
+		let rise_minutes = 0 + rise_time.getMinutes();
+		rise_minutes = addZero(rise_minutes);
+		WEATHER_UI.DETAILS_SUNRISE.textContent = "Sunrise: " + rise_hours + ":" + rise_minutes;
+		let set = (json.sys.sunset);
+		let set_time = new Date(set * 1000);
+		let set_hours = set_time.getHours();
+		set_hours = addZero(set_hours);
+		alert(set_hours);
+		let set_minutes = 0 + set_time.getMinutes();
+		set_minutes = addZero(set_minutes);
+		WEATHER_UI.DETAILS_SUNSET.textContent = "Sunset: " + set_hours + ":" + set_minutes;
+		let srcUrl = `http://openweathermap.org/img/w/${json.weather[0].icon}.png`;
+		WEATHER_UI.NOW_IMG.src = srcUrl;
+		for (let city of WEATHER_UI.CITY_NAME) {
+			city.textContent = json.name;
+		}
+
+	} else {
+		alert("Ошибка HTTP: " + response.status);
+	}
+});
+
